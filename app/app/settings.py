52edbba935fdbab26696833776c87a4953ba6505
Leon <l.beccard@protonmail.com>
2023-02-03 12:10:57 +0100

    Change secret key

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a*0^@18+y99%^*r7f39xj+wqhyo04vgh745)5a#6^og_&vf)9g"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
